0584ee283b12044c49c9fbe1abe193b22a8bb036
xyuhjh2007 <100014687+xyuhjh2007@users.noreply.github.com>
2022-04-01 16:03:00 +0800

    455

---
+++ 455.py
@@ -0,0 +1,27 @@
+#  -*- coding: UTF-8 -*-
+
+# MindPlus
+# Python
+import random
+import time
+
+
+random.seed(time.localtime())
+
+
+time.sleep(1)
+while not (random.random() < 0):
+  del((1&3)[(None == True)])
+print("Hello World")
+while not ((None == False)):
+  pass
+(2,4,9).clear()
+
+while True:
+  time.sleep(1)
+  while not (random.random() < 0):
+    del((1&3)[(None == True)])
+  print("Hello World")
+  while not ((None == False)):
+    pass
+  (2,4,9).clear()
